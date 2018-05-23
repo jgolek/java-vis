7abf1d9a89947ce6485df8b9625faacfe5587991
jacek <jacek.golek@web.de>
2018-05-23 18:58:40 +0200

    merged ViewMain

--- run-test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-gradle clean jar fatJar
-
-java -javaagent:$PWD/build/libs/java-vis-all-0.1.jar -jar build/libs/java-vis-0.1.jar
--- server/src/main/java/org/jg/server/ViewMain.java
+++ server/src/main/java/org/jg/server/ViewMain.java
@@ -1,6 +1,9 @@
 package org.jg.agent.view;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
 
@@ -19,42 +22,59 @@ public class ViewMain {
 		String fileString = FileUtils.readFileToString(new File("test.json"), "UTF-8");
 		JsonArray ja = new JsonArray(fileString);
 
-		
-		
-		for (int i = 0; i < ja.size(); i++) {
-			Item item = new Item();
-			JsonObject obj = (JsonObject) ja.getList().get(i);
-			item.name = obj.getString("name");
-			item.path = "/data/"+i;
-			
-			JsonArray jsonArray = obj.getJsonArray("children", new JsonArray());
-			if(!jsonArray.getList().isEmpty()) {
-				
-			}
-			
-		}
-		
+		final List<Item> items = toItems(ja, "/data/");
 		
 		Vertx vertx = Vertx.vertx();
 		HttpServer server = vertx.createHttpServer();
 
 		Router router = Router.router(vertx);
 
-		router.route("/data/").handler(routingContext -> {
+		router.route("/data").handler(routingContext -> {
 			HttpServerResponse response = routingContext.response();
 			response.putHeader("content-type", "application/json");
 			
-			JsonObject jsonObject = new JsonObject();
-			jsonObject.put("text", "hello");
-			jsonObject.put("isLazy", true);
-			jsonObject.put("isFolder", true);
+			JsonArray jsonArray = new JsonArray();
+			for (Item item : items) {
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.put("text", item.name);
+				jsonObject.put("isLazy", !item.items.isEmpty());
+				jsonObject.put("isFolder", !item.items.isEmpty());		
+				jsonObject.put("lazyUrl", item.path);		
+				
+				jsonArray.add(jsonObject);
+			}
+			
+			response.putHeader("content-type", "application/json");
+			response.end(jsonArray.toString());
+			
+		});
+		
+		router.route("/data/:path").handler(routingContext -> {
+			HttpServerResponse response = routingContext.response();
+			response.putHeader("content-type", "application/json");
+			
+			String param = routingContext.request().getParam("path");
+			System.out.println(param);
+			
+			String[] pathIds = param.split("_");
+			List<Item> searchItems = items;
+			for (int i = 0; i < pathIds.length; i++) {
+				String s = pathIds[i];
+				Item item = searchItems.get(Integer.parseInt(s));
+				searchItems = item.items;
+			}
 			
 			JsonArray jsonArray = new JsonArray();
-			jsonArray.add(jsonObject);
-			jsonArray.add(jsonObject);
+			for (Item item : searchItems) {
+				JsonObject jsonObject = new JsonObject();
+				jsonObject.put("text", item.name);
+				jsonObject.put("isLazy", !item.items.isEmpty());
+				jsonObject.put("isFolder", !item.items.isEmpty());		
+				jsonObject.put("lazyUrl", item.path);		
 
+				jsonArray.add(jsonObject);
+			}
 			
-			System.out.println(jsonArray.toString());
 			 
 			response.putHeader("content-type", "application/json");
 			response.end(jsonArray.toString());
@@ -67,7 +87,23 @@ public class ViewMain {
 		server.requestHandler(router::accept).listen(8080);
 
 		System.out.println("Server is running!");
-		// http://www.easyjstree.com/Demos/LazyLoading
+	}
+
+	private List<Item> toItems(JsonArray ja, String path) {
+		List<Item> items = new ArrayList<Item>();
+		for (int i = 0; i < ja.size(); i++) {
+			Item item = new Item();
+			JsonObject obj = new JsonObject((Map<String, Object>)ja.getList().get(i));
+			item.name = obj.getString("name");
+			item.path = path + i;
+			
+			JsonArray jsonArray = obj.getJsonArray("children", new JsonArray(new ArrayList<>()));
+			if(jsonArray != null && jsonArray.getList() != null && !jsonArray.getList().isEmpty()) {
+				item.items = toItems(jsonArray, item.path + "_");
+			}
+			items.add(item);
+		}
+		return items;
 	}
 
 	public static void main(String[] args) throws Exception {
